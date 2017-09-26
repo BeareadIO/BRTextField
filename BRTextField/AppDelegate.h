f296d516b7cd02749d13e880fddf4f3994938a88
fanjingqi <fanjingqi@bearead.cn>
2017-09-26 20:51:07 +0800

    Bearead Custom TextField Init

---
+++ BRTextField/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRTextField
+//
+//  Created by Archy on 2017/9/26.
+//  Copyright © 2017年 bearead. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
